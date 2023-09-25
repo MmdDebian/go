e466b6ffca42dac29478fb240c07f59a849cb406
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-09-25 22:09:50 +0330

    Change request

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
-package main ;
+package main
 
-func main (){
-	print("Hello world")
+
+// type mytypes bool
+
+// var isAdmin mytypes = true 
+
+func main (){	
+	for index := 0 ; index <= 10 ; index++ {
+		print(index , "\n")
+	}
+
+
+	if  false {
+		print("is admin")
+	} else if 1 != 1 { 
+		print("True")
+	} else {
+		print("not admin")
+	}
 }
+
---
+++ variables/index.go
@@ -0,0 +1,27 @@
+package variables
+
+
+// define variables 
+
+// string type 
+var name string = "ali"
+
+
+// number types : 
+var number int = 15 
+
+var number1 int8 = 15 
+
+var number2 int16 = 115 
+
+var number3 int32 = 15 
+
+var number4 int64 = 15 
+
+
+// declare constant 
+
+const username string = "mamadi"
+
+const age int = 20 
+
