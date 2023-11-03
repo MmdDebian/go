8145748dcf851ba0adab01380dea685edf7dfeb4
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-11-03 21:52:25 +0330

    modified slice

--- slices/slice.go
+++ slices/slice.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	var s []string
@@ -36,6 +39,34 @@ func main() {
 	fmt.Println(c)
 
 	l := s[2:5]
-
 	fmt.Println("sl1:" , l)
+
+	l = s[:5]
+	fmt.Println("sl2:" , l)
+
+	l = s[2:]
+    fmt.Println("sl3:", l)
+
+	t := []string{"g","h","i"}
+    fmt.Println("dcl:" , t)
+
+	t2 := []string{"g","h","i"}
+
+	if slices.Equal(t , t2) {
+		fmt.Println("t == t2")
+	}
+
+	twoD := make([][]int , 3)
+
+	for i := 0; i < 3 ; i++ {
+		innerLen := i + 1 
+
+		twoD[i] = make([]int , innerLen)
+
+		for j := 0 ; j < innerLen ; j++ {
+			twoD[i][j] = i + j 
+		}
+	}
+
+	fmt.Println("2d :" , twoD)
 }
