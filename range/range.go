472641a0127e45aba8730119e814bb4db39cc7e8
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-12-18 23:04:09 +0330

    go ranges (use range for maps)

--- range/range.go
+++ range/range.go
@@ -13,5 +13,27 @@ func main() {
 
 	fmt.Println("sum : " , sum)
 
+
+	for i , num := range nums {
+		if num == 3 {
+			fmt.Println("Index : " , i)
+		}
+	}
+
+	kvs := map[string]string{"a":"apple","b":"banana"}
+
+
+	for k , v := range kvs {
+		// use range for key and value 
+		fmt.Printf("%s -> %s\n" ,k ,v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key: " ,k)
+	}
+
 	
+	for i , c := range "go" {
+		fmt.Println(i , c)
+	}
 }
