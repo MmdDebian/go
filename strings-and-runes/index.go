20c6a71f4a359608e264ad3543c3f31410882066
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-12-31 22:43:21 +0330

    go : strings and runes

---
+++ strings-and-runes/index.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	const s = "สวัสดี"
+
+	fmt.Println("Len:", len(s))
+
+	for i := 0 ; i < len(s) ; i++ {
+		fmt.Printf("%x" , s[i])
+	}
+
+	fmt.Println()
+
+	fmt.Println("Rune count : " , utf8.RuneCountInString(s))
+}
