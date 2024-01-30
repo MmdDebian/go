32f3718ce2a580a4029baba615267c834ecd9e2f
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-01-30 21:35:36 +0330

    editor challenge 1

--- devops-hobbies/part4/main.go
+++ devops-hobbies/part4/main.go
@@ -32,6 +32,8 @@ func main() {
 	reader := bufio.NewReader(os.Stdin) 
 
 
+	curser := &element{data: '|'}
+
 	for index := 0; index < count; index++ {
 		line, _ := reader.ReadString('\n')
 		line = strings.TrimSuffix(line , "\n")
@@ -39,17 +41,28 @@ func main() {
 
 		switch command(fields[0]){
 			case INSERT :
-				fmt.Println("INSERT")
+				element := element{data: []byte(fields[1])[0]}
 
-			case LEFT : 
-				fmt.Println("LEFT")
+				curser.prev
 
+			case LEFT :
+				if curser.prev != nil {
+					curser.prev.data,curser.data = curser.data , curser.prev.data
+					curser = curser.prev
+				}
 			case RIGHT :
-				fmt.Println("RIGHT")
+					if curser.next != nil {
+					curser.next.data,curser.data = curser.data , curser.next.data
+					curser = curser.next
+				}
 
 			case BACKSPACE :
-				fmt.Println("BACKSPACE")
-				
+				if curser.prev.prev != nil{
+					curser.prev.prev.next = curser
+					curser.prev = curser.prev.prev 
+				}else {
+					curser.prev = nil 
+				}
 			default :
 				panic("invalid command given")
 		}
