2cbf0ad2a7b1f800dc086f84bf95bc823b42c809
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-13 12:47:10 +0330

    set and get reis

--- devops-hobbies/repeat/app/main.go
+++ devops-hobbies/repeat/app/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -14,7 +14,22 @@ func main(){
 		DB: 0,
 	})
 
-	ctx := context.Background()
+        ctx := context.Background()
 
-	client.Set(ctx , "name" , "ali",0)
+	// client.Set(ctx , "name" , "ali",0)
+
+	session := map[string]string{"name":"mamad","username":"mohamad20"}
+
+	for k,v := range session {
+		err := client.HSet(ctx , "user-session:123",k,v).Err()
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	userSession := client.HGetAll(ctx,"user-session:123").Val()
+
+	fmt.Println(userSession)
+
 }
