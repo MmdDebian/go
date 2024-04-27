2b783d9ef915682333b5943f08f5d09dc636b6b1
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-04-27 22:18:57 +0330

    Learn arrays

--- go-examples/arrays/array.go
+++ go-examples/arrays/array.go
@@ -1,30 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mohamadmirzaeidev/go/arrays/tool"
+)
 
 func main() {
 	var a [5]int
 
-	fmt.Println("emp :" , a)
+	fmt.Println("emp :", a)
 
-	a[4] = 100 
+	a[4] = 100
 
-	fmt.Println("set :" , a)
+	fmt.Println("set :", a)
 
-	fmt.Println("get :" , a[4])
+	fmt.Println("get :", a[4])
 
-	fmt.Println("len :" , len(a))
+	fmt.Println("len :", len(a))
 
-	b := [5]int{1,2,3,4}
+	b := [5]int{1, 2, 3, 4}
 
-	fmt.Println("dcl :" , b)
+	fmt.Println("dcl :", b)
 
-	var twoD [2][3]int 
+	var twoD [2][3]int
 
-	for i := 0; i < 2 ; i++ {
-		for j := 0 ; j < 3; j++ {
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
 			twoD[i][j] = i + j
 		}
 	}
-	fmt.Println("2d:" , twoD)
+	fmt.Println("2d:", twoD)
+
+	arr := []string{"test", "mamad"}
+	tool.Map(arr)
 }
---
+++ go-examples/arrays/tool/map.go
@@ -0,0 +1,9 @@
+package tool
+
+import "fmt"
+
+func Map(arr []string) {
+	for _ , v := range arr {
+		fmt.Println(v)
+	}
+}
