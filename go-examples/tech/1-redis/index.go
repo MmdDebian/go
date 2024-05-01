a0bbd9c9c18366f0588bce10df855cd06871ae04
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-05-02 00:10:26 +0330

    (#4) add packages (step by step v1)

--- go-examples/tech/1-redis/index.go
+++ go-examples/tech/1-redis/index.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("key", val)
 }
